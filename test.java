973977c7f2b77924c6de6de47e8629420eab9aed
pmasta <piotr.krzysztof.maslanka@gmail.com>
2025-05-21 11:56:12 +0200

    Update test.java

--- test.java
+++ test.java
@@ -1,15 +1,9 @@
-import os
+import pysqlite3
 import sys
 import subprocess
 
-PYTHON_CODE = """
-import pysqlite3
-import sys
-sys.modules['sqlite3'] = pysqlite3
-import chromadb.cli
-"""
+sys.modules["sqlite3"] = pysqlite3
 
-# Uruchom chroma run z podmienionym sqlite3
 subprocess.run([
-    sys.executable, "-c", PYTHON_CODE, "run", "--path", "./chroma_data"
+    sys.executable, "-m", "chromadb.cli", "run", "--path", "./chroma_data"
 ])
